e742e875985fc12487d63a4dce1be6856f41a22c
github-action-benchmark <github@users.noreply.github.com>
2025-03-15 01:00:19 +0000

    add Rust Benchmark (cargo) benchmark result for 41a39930a0a5dc15bb34f624f47c4f18fd2d88b6

---
+++ docs/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1742000419603,
+  "repoUrl": "https://github.com/skanehira/yui",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "committer": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "distinct": true,
+          "id": "41a39930a0a5dc15bb34f624f47c4f18fd2d88b6",
+          "message": "init",
+          "timestamp": "2025-03-15T09:57:47+09:00",
+          "tree_id": "4f345d91835a21713efe869cfd6f911185799382",
+          "url": "https://github.com/skanehira/yui/commit/41a39930a0a5dc15bb34f624f47c4f18fd2d88b6"
+        },
+        "date": 1742000418980,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.02,
+            "range": "± 0.10",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
