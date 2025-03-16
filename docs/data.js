395b7584493815e658cdbd1943ffbc8bb9a396ac
github-action-benchmark <github@users.noreply.github.com>
2025-03-16 09:37:13 +0000

    add Rust Benchmark (cargo) benchmark result for e32976ad39be18e342802e126e9713410b6a62e6

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742116153403,
+  "lastUpdate": 1742117833916,
   "repoUrl": "https://github.com/skanehira/yui",
   "entries": {
     "Rust Benchmark": [
@@ -89,6 +89,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 91.98,
             "range": "± 18.91",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "committer": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "distinct": true,
+          "id": "e32976ad39be18e342802e126e9713410b6a62e6",
+          "message": "refactor: ELF parsing and error handling\n\n- Renamed various ELF-related enums and structs for clarity:\n  - `ElfClass` to `Class`\n  - `ElfData` to `Data`\n  - `ElfIdentVersion` to `IdentVersion`\n  - `ElfOSABI` to `OSABI`\n  - `ElfType` to `Type`\n  - `ElfMachine` to `Machine`\n  - `ElfVersion` to `Version`\n  - `ElfHeader` to `Header`\n  - `ElfIdent` to `Ident`\n  - `ElfError` to `ParseError`\n- Updated `parse_elf_header` function to return `Header` instead of `ElfHeader`.\n- Added `parse_elf` function to parse the entire ELF structure.\n- Modified tests to reflect these changes.\n- Updated benchmark function name to `bench_parse_elf_header`.",
+          "timestamp": "2025-03-16T18:36:28+09:00",
+          "tree_id": "95fb8c5a98ef7eef9e9c32417bc065ddc2bf7fb0",
+          "url": "https://github.com/skanehira/yui/commit/e32976ad39be18e342802e126e9713410b6a62e6"
+        },
+        "date": 1742117833599,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_parse_elf_header",
+            "value": 90.66,
+            "range": "± 1.35",
             "unit": "ns/iter"
           }
         ]
