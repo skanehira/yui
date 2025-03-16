dfe63d2f9c638744a656026554c2e69ea6891696
github-action-benchmark <github@users.noreply.github.com>
2025-03-16 07:20:51 +0000

    add Rust Benchmark (cargo) benchmark result for 6b149aba3db4f353ec39a3c5680534dc41321e3c

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742000419603,
+  "lastUpdate": 1742109651755,
   "repoUrl": "https://github.com/skanehira/yui",
   "entries": {
     "Rust Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 18.02,
             "range": "± 0.10",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "committer": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "distinct": true,
+          "id": "6b149aba3db4f353ec39a3c5680534dc41321e3c",
+          "message": "impl: parse elf header",
+          "timestamp": "2025-03-16T16:20:10+09:00",
+          "tree_id": "c6c6c5d2d43e4e1059ed64e337b35202b41d942a",
+          "url": "https://github.com/skanehira/yui/commit/6b149aba3db4f353ec39a3c5680534dc41321e3c"
+        },
+        "date": 1742109651421,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.02,
+            "range": "± 0.44",
             "unit": "ns/iter"
           }
         ]
