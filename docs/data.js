1580a67c8461d1c3f991c040b6aa4604c3e439a2
github-action-benchmark <github@users.noreply.github.com>
2025-03-16 09:09:13 +0000

    add Rust Benchmark (cargo) benchmark result for 90e66c377d4eb3a85a3174a966dcf11896523c05

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742109651755,
+  "lastUpdate": 1742116153403,
   "repoUrl": "https://github.com/skanehira/yui",
   "entries": {
     "Rust Benchmark": [
@@ -59,6 +59,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 18.02,
             "range": "± 0.44",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "committer": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "distinct": true,
+          "id": "90e66c377d4eb3a85a3174a966dcf11896523c05",
+          "message": "chore: refactor something\n\n- add Makefile\n- add bench test\n- simplify test object file",
+          "timestamp": "2025-03-16T18:08:27+09:00",
+          "tree_id": "85a3339e0f26810381834a95faf3cbc2d3b29443",
+          "url": "https://github.com/skanehira/yui/commit/90e66c377d4eb3a85a3174a966dcf11896523c05"
+        },
+        "date": 1742116152899,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 91.98,
+            "range": "± 18.91",
             "unit": "ns/iter"
           }
         ]
