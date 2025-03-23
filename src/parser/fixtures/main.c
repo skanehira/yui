d816730002fcbaa0561105e7f7a45c9eeb997649
skanehira <sho19921005@gmail.com>
2025-03-23 11:09:08 +0900

    chore: simplify main.c

--- src/parser/fixtures/main.c
+++ src/parser/fixtures/main.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-extern int x;
-
-int main() {
-	printf("x = %d\n", x);
-}
+__asm__(".global _start\n"
+        "_start:\n"
+        "    adr     x0, x\n" // x のアドレスを、PC 相対の adr 命令で直接取得
+        "    ldr     w0, [x0]\n" // x の値 (32ビット, 例: 23) を x0 にロード（w0 で読み込む）
+        "    mov     x8, #93\n" // exit システムコール番号 93 を x8 にセット
+        "    svc     #0\n" // システムコールを発行して終了
+);
